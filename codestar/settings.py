e8f89ddbb5ccc6a1620301369d7cf6a6b9358037
Pimmz <>
2023-10-20 10:42:13 +0000

    Add styling to image boxes

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-pimmz-project-4-78d1tm6wx9.us2.codeanyapp.com',
                  'pimmz-project-4-9cc2ab59cc64.herokuapp.com']
